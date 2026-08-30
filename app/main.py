a477037a3168d9681f6a15451bc49fd5f9f6d70a
Raphi Lutier <rlutier8*@icloud.com>
2026-08-30 18:21:13 +0100

    Added first version of recommendation algorithm

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 #python -m uvicorn app.main:app --reload
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from .database import Base, engine, get_db
 from . import models, schemas
+from .recommender import genre_counter, get_liked_genres, score_movies, get_recommendations
 
 Base.metadata.create_all(bind=engine)
 
@@ -50,3 +51,73 @@ def create_movie(movie: schemas.MovieCreate, db: Session = Depends(get_db)):
 @app.get("/movies")
 def get_movies(db: Session = Depends(get_db)):
     return db.query(models.Movie).all()
+
+@app.post("/users/{user_id}/likes/{movie_id}")
+def like_movie(user_id: int, movie_id: int, db: Session = Depends(get_db)):
+    movie = db.query(models.Movie).filter(models.Movie.id == movie_id).first()
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    #For errors
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if movie is None:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    
+    new_like = models.Like(
+        user_id = user.id,
+        movie_id = movie.id
+    )
+
+    db.add(new_like)
+    db.commit()
+    db.refresh(new_like)
+
+    return new_like
+
+@app.post("/users/{user_id}/watches/{movie_id}")
+def watch_movie(user_id:int, movie_id: int, db: Session = Depends(get_db)):
+    movie = db.query(models.Movie).filter(models.Movie.id == movie_id).first()
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    #For errors
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if movie is None:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    
+    new_watch = models.Watch(
+        user_id = user.id,
+        movie_id = movie.id
+    )
+
+    db.add(new_watch)
+    db.commit()
+    db.refresh(new_watch)
+
+    return new_watch
+
+@app.get("/users/{user_id}/likes")
+def get_likes(user_id: int, db: Session = Depends(get_db)):
+    return db.query(models.Like).filter(models.Like.user_id == user_id).all()
+
+@app.get("/users/{user_id}/watches")
+def get_watches(user_id: int, db: Session = Depends(get_db)):
+    return db.query(models.Watch).filter(models.Watch.user_id == user_id).all()
+
+@app.get("/users/{user_id}/recommendations")
+def get_user_recommendations(user_id: int, db: Session = Depends(get_db)):
+
+   user = db.query(models.User).filter(models.User.id == user_id).first()
+
+   if user is None:
+       raise HTTPException(status_code=404, detail="User not found")
+   
+   movies = db.query(models.Movie).all()
+
+   genre_counts = get_liked_genres(user)
+
+   recommendations = get_recommendations(movies, genre_counts, user)
+
+   return recommendations
---
+++ app/recommender.py
@@ -0,0 +1,57 @@
+def genre_counter(liked_movies):
+    genre_counts = {}
+
+    for movie in liked_movies:
+        genres = movie.split(", ")
+        for genre in genres:
+            if genre in genre_counts:
+                genre_counts[genre] += 1
+            else:
+                genre_counts.update({genre: 1})
+    
+    return genre_counts
+
+def get_liked_genres(user):
+    liked_movies = []
+
+    for like in user.likes:
+        genres = like.movie.genres
+        liked_movies.append(genres)
+
+    return genre_counter(liked_movies)
+
+
+def score_movies(movie, genre_counts):
+    score = 0
+
+    genres = movie.genres.split(", ")
+
+    for genre in genres:
+        if genre in genre_counts:
+            score += genre_counts[genre]
+
+    return score
+
+def get_recommendations(movies, genre_counts, user):
+
+    #Get watches / likes
+    watched_or_liked = set()
+
+    for like in user.likes:
+        watched_or_liked.add(like.movie_id)
+
+    for watch in user.watches:
+        watched_or_liked.add(watch.movie_id)
+        
+    recommendations = []
+
+    for movie in movies:
+        if movie in watched_or_liked:
+            continue
+        score = score_movies(movies, genre_counts)
+
+        recommendations.append({"movie": movie, "score": score})
+
+    recommendations.sort(key=lambda x: x["score"], reverse="True")
+
+    return recommendations
